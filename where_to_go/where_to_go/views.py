e9d5107312ff3a1d8fead96955f48dc37f774c24
Aleksy <aslepaugo@gmail.com>
2023-01-06 14:10:39 +0100

    Move data from template to view

--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    geo = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+          }
+        }
+      ]
+    }
+    context = {"places": geo}
+    return TemplateResponse(request, "index.html", context)
